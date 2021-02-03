daadda3de3b2b84c712fe81aa21ce949e7a2b354
dmj1234 <1165689381@qq.com>
2021-02-03 23:28:52 +0800

    修复login组件无法渲染问题

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Login from '@/components/Login'
+import Login from '../components/Login'
+
 Vue.use(VueRouter)
 
-export default new VueRouter({
-  routes: [
-    {
-      path: '/', redirect: '/login'
-    },
-    {
-      path: './login', component: Login
-    }
-  ]
+const routes = [
+  {
+    path: '/',
+    redirect: '/login'
+  },
+  {
+    path: '/login',
+    component: Login
+  }
+]
+
+const router = new VueRouter({
+  routes
 })
+
+export default router
