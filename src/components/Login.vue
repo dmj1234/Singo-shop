1c6846fda789a0f42e290a6078746cf279a1df47
dmj1234 <1165689381@qq.com>
2021-02-04 16:27:21 +0800

    重置表单

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -7,7 +7,7 @@
    </div>
 
 <!--   登录表单区域-->
-   <el-form :model="loginForm"  :rules="loginFormRules" label-width="0px" class="login_form">
+   <el-form ref="loginFormRef" :model="loginForm"  :rules="loginFormRules" label-width="0px" class="login_form">
 
 <!--     用户名-->
      <el-form-item  prop="username" >
@@ -22,7 +22,7 @@
 <!--   按钮区域-->
    <el-form-item  class="bts">
      <el-button type="primary">登录</el-button>
-     <el-button type="info">重置</el-button>
+     <el-button type="info" @click="resetLoginForm">重置</el-button>
    </el-form-item>
    </el-form>
  </div>
@@ -51,6 +51,13 @@ export default {
           { min: 3, max: 12, message: '长度在 3 到 12 个字符', trigger: 'blur' }
         ]
       }
+    }
+  },
+  // 点击重置按钮，重置登录表单
+  methods: {
+    resetLoginForm () {
+      // console.log(this)
+      this.$refs.loginFormRef.resetFields()
     }
   }
 }
