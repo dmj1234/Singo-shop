f9d9a8019e30e6a19d9ec81f3abd6d570ae08bc6
dmj1234 <1165689381@qq.com>
2021-02-05 23:35:22 +0800

    登录功能实现

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -70,7 +70,9 @@ export default {
         // 1.将登录成功之后的token，保存到客户端的 sessionStorage
         // 1.1项目中除了登录之外的其他API接口，必须在登录之后才能访问
         // 1.2token 只应在当前网站打开期间生效，所以将token保存在sessionStorage中
+        window.sessionStorage.setItem('token', res.data.token)
         // 2.通过编程式导航跳转到后台主页，路由地址是 /home
+        this.$router.push('/home')
       })
     }
   }
--- src/main.js
+++ src/main.js
@@ -11,7 +11,6 @@ import axios from 'axios'
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 Vue.prototype.$http = axios
 Vue.config.productionTip = false
-
 new Vue({
   router,
   render: h => h(App)
