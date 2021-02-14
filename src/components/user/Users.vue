87d4888efcdb32f3b6b73f7141dc60817f39b945
dmj1234 <1165689381@qq.com>
2021-02-14 13:53:39 +0800

    添加table渲染

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -19,6 +19,15 @@
           <el-button type="primary">添加用户</el-button>
         </el-col>
       </el-row>
+<!--      用户列表区域-->
+      <el-table :data="userList" border stripe>
+        <el-table-column label="姓名" prop="username"></el-table-column>
+        <el-table-column label="邮箱" prop="email"></el-table-column>
+        <el-table-column label="电话" prop="mobile"></el-table-column>
+        <el-table-column label="角色" prop="role_name"></el-table-column>
+        <el-table-column label="状态" prop="mg_state"></el-table-column>
+        <el-table-column label="操作" ></el-table-column>
+      </el-table>
     </el-card>
 
   </div>
@@ -26,6 +35,31 @@
 
 <script>
 export default {
+  data() {
+    return {
+      // 获取用户列表的参数对象
+      queryInfo: {
+        query: '',
+        pagenum: 1,
+        pagesize: 2
+      },
+      userList: [],
+      total: 0
+    }
+  },
+  created() {
+    this.getUserList()
+  },
+  methods: {
+    async getUserList() {
+      const { data:res } = await this.$http.get('users', { params: this.queryInfo })
+      if (res.meta.status !== 200) return this.$message.error('获取用户列表失败')
+      this.userList = res.data.users
+      this.total = res.data.total
+
+      console.log(res)
+    }
+  }
 
 }
 </script>
