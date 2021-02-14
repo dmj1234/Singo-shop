249abf94b34ab01e829e26552f6f396b8bfe099e
dmj1234 <1165689381@qq.com>
2021-02-14 22:18:45 +0800

    权限列表展示

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,8 @@ import Login from '../components/Login'
 import Home from '../components/Home'
 import Welcome from '../components/Welcome'
 import Users from '@/components/user/Users'
+import Rights from '../components/power/Rights'
+import Roles from '../components/power/Roles'
 Vue.use(VueRouter)
 
 const router = new VueRouter({
@@ -20,7 +22,11 @@ const router = new VueRouter({
       path: '/home',
       redirect:'/welcome',
       component:Home,
-      children: [{ path:'/welcome', component:Welcome }, { path:'/users', component:Users }
+      children: [
+        { path:'/welcome', component:Welcome },
+        { path:'/users', component:Users },
+        { path:'/rights', component:Rights },
+        { path:'/roles', component:Roles }
       ]
     }
   ]
