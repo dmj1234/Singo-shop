7d61600a42a21bf9ba32375833abf59cc2552a93
dmj1234 <1165689381@qq.com>
2021-02-14 15:10:19 +0800

    添加表单渲染

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -56,10 +56,23 @@
       </el-pagination>
 <!--添加用户的对话框-->
       <el-dialog
-        title="提示"
+
         :visible.sync="addDialogVisible"
         width="30%">
-        <span>这是一段信息</span>
+        <el-form :model="addForm" :rules="addFormRules" ref="addFormRef" label-width="70px" >
+          <el-form-item label="用户名" prop="username">
+            <el-input v-model="addForm.username"></el-input>
+          </el-form-item>
+          <el-form-item label="密码" prop="password">
+            <el-input v-model="addForm.password"></el-input>
+          </el-form-item>
+          <el-form-item label="邮箱" prop="email">
+            <el-input v-model="addForm.email"></el-input>
+          </el-form-item>
+          <el-form-item label="手机" prop="mobile">
+            <el-input v-model="addForm.mobile"></el-input>
+          </el-form-item>
+          </el-form>
 <!--        底部区域-->
         <span slot="footer" class="dialog-footer">
     <el-button @click="addDialogVisible = false">取 消</el-button>
@@ -83,7 +96,33 @@ export default {
       },
       userList: [],
       total: 0,
-      addDialogVisible: false
+      addDialogVisible: false,
+      addForm: {
+        username: '',
+        password:''
+      },
+      addFormRules: {
+        username: [
+          { require: true, message:'请输入用户名', trigger:'blur' }, {
+            min: 3, max: 10, message: '用户名的长度在3-10个字符之间', trigger: 'blur'
+          }
+        ],
+        password: [
+          { require: true, message:'请输入密码', trigger:'blur' }, {
+            min: 6, max: 15, message: '用户名的长度在3-10个字符之间', trigger: 'blur'
+          }
+        ],
+        email: [
+          { require: true, message:'请输入邮箱', trigger:'blur' }, {
+            min: 6, max: 15, message: '用户名的长度在3-10个字符之间', trigger: 'blur'
+          }
+        ],
+        mobile: [
+          { require: true, message:'请输入手机', trigger:'blur' }, {
+            min: 6, max: 15, message: '用户名的长度在3-10个字符之间', trigger: 'blur'
+          }
+        ]
+      }
     }
   },
   created() {
